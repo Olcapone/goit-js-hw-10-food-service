0e117e1e0d91ff2280f1968640f3573090dfdadb
OLgaPonomarenko <fblackberries@gmail.com>
2021-06-17 14:23:24 +0300

    старт

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-import './sass/main.scss';
+'use strict';
+
+import './styles.css';
+import template from './userList.hbs';
+
+
+const products = require('./menu.json');
+
+
+const mainMenue = document.querySelector(".js-menu");
+mainMenue.insertAdjacentHTML('beforeend', template(products));
+
+const buttonTheme = document.querySelector('input');
+const allPage = document.querySelector('body');
+
+
+
+buttonTheme.addEventListener('focus', switchTheme);
+
+
+function switchTheme(e) {
+
+     allPage.classList.toggle('dark-theme');
+
+ }
+
+
+
+
