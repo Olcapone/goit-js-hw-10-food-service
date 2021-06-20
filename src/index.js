dc1d1008b9382060118a9e7bc3ffea86951e543a
OLgaPonomarenko <fblackberries@gmail.com>
2021-06-20 15:38:19 +0300

    сохранение темі исправлено

--- src/index.js
+++ src/index.js
@@ -10,19 +10,48 @@ const products = require('./menu.json');
 const mainMenue = document.querySelector(".js-menu");
 mainMenue.insertAdjacentHTML('beforeend', template(products));
 
-const buttonTheme = document.querySelector('input');
+const buttonTheme = document.querySelector('#theme-switch-toggle');
 const allPage = document.querySelector('body');
 
 
 
-buttonTheme.addEventListener('focus', switchTheme);
+buttonTheme.addEventListener('change', switchTheme);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+}
 
 
-function switchTheme(e) {
+function switchTheme(e) {   
+  
+     if (!e.target.checked){
+        allPage.classList.add(Theme.LIGHT);
+        allPage.classList.remove(Theme.DARK);
+        localStorage.setItem('Theme', Theme.LIGHT);
+    }
+      else {
+          
+         allPage.classList.add(Theme.DARK);
+         allPage.classList.remove(Theme.LIGHT);
+         localStorage.setItem('Theme', Theme.DARK);
+    }
 
-     allPage.classList.toggle('dark-theme');
+};
 
- }
+ savedTheme();
+
+function savedTheme() {
+  const saveTheme = localStorage.getItem('Theme');
+  if (saveTheme) {
+    allPage.classList.add(saveTheme);
+     if (saveTheme === Theme.DARK) {
+      buttonTheme.checked = true;
+     }
+  }
+}
+     
+
 
 
 
